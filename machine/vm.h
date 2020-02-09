ff40723d3dcaf068adddad93c6764f69d04681e8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-09 14:19:19 +0000

    Introduce an addr_t type

--- machine/vm.h
+++ machine/vm.h
@@ -11,12 +11,14 @@
 # define SATP_MODE_CHOICE INSERT_FIELD(0, SATP64_MODE, SATP_MODE_SV39)
 # define VA_BITS 39
 # define GIGAPAGE_SIZE (MEGAPAGE_SIZE << RISCV_PGLEVEL_BITS)
+typedef uint64_t addr_t;
 #else
 # define SATP_MODE_CHOICE INSERT_FIELD(0, SATP32_MODE, SATP_MODE_SV32)
 # define VA_BITS 32
+typedef uint32_t addr_t;
 #endif
+typedef addr_t pte_t;
 
-typedef uintptr_t pte_t;
 extern pte_t* root_page_table;
 
 static inline void flush_tlb()
@@ -24,12 +26,12 @@ static inline void flush_tlb()
   asm volatile ("sfence.vma");
 }
 
-static inline pte_t pte_create(uintptr_t ppn, int type)
+static inline pte_t pte_create(addr_t ppn, int type)
 {
   return (ppn << PTE_PPN_SHIFT) | PTE_V | type;
 }
 
-static inline pte_t ptd_create(uintptr_t ppn)
+static inline pte_t ptd_create(addr_t ppn)
 {
   return pte_create(ppn, PTE_V);
 }
